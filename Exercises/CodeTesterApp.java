353afe6b59967791c03844dc18f03c7405bc749b
LJBenitez17 <58523153+LJBenitez17@users.noreply.github.com>
2020-01-09 12:22:30 -0600

    Add files via upload

---
+++ Exercises/CodeTesterApp.java
@@ -0,0 +1,32 @@
+package murach.test;                                   //Luke Benitez
+
+public class CodeTesterApp {
+
+    public static void main(String args[]) {
+        // display a welcome message
+        System.out.println("Welcome to the Code Tester");
+        System.out.println();
+
+        // calculate and display a total
+        String productCode = "java";
+        double price = 49.50;
+        int quantity = 2;
+        double total = price * quantity;
+        String message = 
+            "Code:     " + productCode + "\n" +
+            "Price:    " + price + "\n" +
+            "Quantity: " + quantity + "\n" +
+            "Total:    " + total + "\n";
+        System.out.println(message);
+
+        // display your name        
+        // TODO: add code here
+        System.out.println("My name is Luke");
+        // calculate and display an average        
+        // TODO: add code here
+        total/quantity = sum
+        		System.out.println("Sum = " sum)
+        // display a goodbye message
+        System.out.println("Bye!");
+    }
+}
